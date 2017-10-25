b0706f3a1daba0a6e7dcd20c4b8a5a91b2fed9ab
Dennis Dunn <ansofive@gmail.com>
2017-10-24 22:18:21 -0400

    I was toggling the data line instead of the clock line. Oops.

--- lib/blinkt.ex
+++ lib/blinkt.ex
@@ -16,67 +16,82 @@ defmodule Blinkt do
 
   def set_brightness(brightness) do
     for i <- 0..7, do: Pixels.set_pixel(i, brightness) 
+    :ok
   end
 
   def clear() do
     for i <- 0..7, do: Pixels.set_pixel(i, %Pixel{lux: 0.2})
+    :ok
   end
 
   def show() do
     _sof()
     for i <- 0..7, do:  _write_pixel(Pixels.get(i))
     _eof()
+    :ok
   end
 
   def get_pixel(x) do
     Pixels.get(x)
   end
 
+  def set_pixel(x, pixel) do
+    Pixels.put(x, pixel)
+  end
+
   def set_pixel(x, r, g, b, brightness) do
-    Pixels.put(x, %Pixel{red: r, green: g, blue: b, lux: brightness})
+    Pixels.put(x, %Pixel{red: r, green: g, blue: b, lux: trunc(brightness * 31) &&& 31})
     :ok
   end
 
   def set_all(r, g, b, brightness) do 
     for i <- 0..7, do: Blinkt.set_pixel(i, r, g, b, brightness)
+    :ok
   end
 
   defp _sof do
-    for _ <- 1..32, do: _pulse_gpio_pin(@dat)
+    GPIO.write(@dat, 0)
+    for _ <- 1..32, do: _pulse_gpio_pin(@clk)
+    :ok
   end
 
   defp _eof do
-    for _ <- 1..36, do: _pulse_gpio_pin(@dat)
+    GPIO.write(@dat, 0)
+    for _ <- 1..36, do: _pulse_gpio_pin(@clk)
+    :ok
   end
 
   defp _write_pixel(pixel) do
-    GPIO.write(@dat, 0)
-    GPIO.write(@clk, 0)
     %Pixel{red: r, green: g, blue: b, lux: brightness} = pixel
-    _write_byte(<<trunc(brightness * 31) &&& 31>>)
+    _write_byte(<<brightness ||| 224>>)
     _write_byte(<<b>>)
     _write_byte(<<g>>)
     _write_byte(<<r>>)
+    :ok
   end
 
   defp _write_byte(bits) when bits == <<>> do
     :ok
   end
+
   defp _write_byte(bits) do
     <<b::size(1), rest::bitstring>> = bits
     GPIO.write(@dat, b)
     _pulse_gpio_pin(@clk)
     _write_byte(rest)
+    :ok
   end
 
   defp _pulse_gpio_pin(pin) do
     GPIO.write(pin, 1)
     GPIO.write(pin, 0)
+    :ok
   end
 
   defp _setup_gpio do
     GPIO.set_mode(@dat, :output)
     GPIO.set_mode(@clk, :output)
+    :ok
   end
   
 end
